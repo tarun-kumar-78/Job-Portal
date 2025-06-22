dfe74d92821fceff446b47db8bec9119509d279c
User2003r <itarun78072@gmail.com>
2025-06-22 14:48:12 +0530

    Job History Pages

---
+++ frontend/src/JobHistory/Card.tsx
@@ -0,0 +1,87 @@
+import { Button, Divider, Text } from "@mantine/core";
+import {
+  IconBookmark,
+  IconBookmarkFilled,
+  IconCalendarMonth,
+  IconClockHour3,
+} from "@tabler/icons-react";
+import { Link } from "react-router-dom";
+
+const Card = (props: any) => {
+  return (
+    <Link
+      to="/jobs"
+      className="w-80 p-4 flex flex-col gap-4 rounded-xl bg-mine-shaft-900 cursor-pointer hover:shadow-[0_0_5px_1px] !shadow-bright-sun-400"
+    >
+      <div className="flex justify-between">
+        <div className="flex gap-4 items-center">
+          <div>
+            <img
+              src={props.company}
+              className="w-10 bg-mine-shaft-700 p-1 rounded-md"
+              alt="Meta"
+            />
+          </div>
+          <div className="">
+            <div className="text-lg text-mine-shaft-300 font-semibold">
+              {props.title}
+            </div>
+          </div>
+        </div>
+        <div>
+          {!props.saved ? (
+            <IconBookmark className="w-5 h-5 text-mine-shaft-300" />
+          ) : (
+            <IconBookmarkFilled className="w-5 h-5 text-bright-sun-400" />
+          )}
+        </div>
+      </div>
+      <div className="flex gap-2 [&>div]:bg-mine-shaft-800 [&>div]:rounded-md [&>div]:w-fit [&>div]:py-1 [&>div]:px-2 [&>div]:text-bright-sun-400 [&>div]:text-xs">
+        <div>{props.jobType}</div>
+        <div>{props.experience}</div>
+        <div>{props.location}</div>
+      </div>
+      <div>
+        <Text lineClamp={2} className="!text-xs text-justify">
+          {props.description}
+        </Text>
+      </div>
+      <Divider size="xs" color="mineShaft.7" />
+      <div className="flex justify-between text-xs">
+        <div className="text-sm font-semibold">&#8377; 24 LPA</div>
+        <div className="flex gap-1 text-mine-shaft-500 items-center">
+          <IconClockHour3 className="w-5 h-5" />
+
+          {props.applied || props.interviewing
+            ? "Applied "
+            : props.offered
+            ? "Interview "
+            : "Posted "}
+          {props.postedDaysAgo}
+        </div>
+      </div>
+      {props.offered ||
+        (props.interviewing && <Divider size="xs" color="mineShaft.7" />)}
+      {props.offered && (
+        <div className="flex gap-2">
+          <Button color="brightSun.4" variant="outline" fullWidth>
+            Accept
+          </Button>
+          <Button color="brightSun.4" variant="light" fullWidth>
+            Reject
+          </Button>
+        </div>
+      )}
+
+      {props.interviewing && (
+        <div className="flex gap-2 text-mine-shaft-200 text-sm items-center">
+          <IconCalendarMonth stroke={1.5} className="text-bright-sun-400" />{" "}
+          Sat, August 23 &bull;
+          <span className="text-mine-shaft-400">10:00 AM</span>
+        </div>
+      )}
+    </Link>
+  );
+};
+
+export default Card;
---
+++ frontend/src/JobHistory/JobHistory.tsx
@@ -0,0 +1,50 @@
+import { Tabs } from "@mantine/core";
+import { jobs } from "../Data/data";
+import Card from "./Card";
+
+const JobHistory = () => {
+  return (
+    <div>
+      <div className="text-2xl font-semibold py-2">Job History</div>
+      <Tabs defaultValue="About" className="[&_button]:text-lg font-semibold">
+        <Tabs.List>
+          <Tabs.Tab value="applied">Applied</Tabs.Tab>
+          <Tabs.Tab value="saved">Saved</Tabs.Tab>
+          <Tabs.Tab value="offered">Offered</Tabs.Tab>
+          <Tabs.Tab value="interviewing">Interviewing</Tabs.Tab>
+        </Tabs.List>
+        <Tabs.Panel value="applied">
+          <div className="mt-10 flex gap-5 flex-wrap px-20">
+            {jobs.map((job, index) => (
+              <Card key={index} applied {...job} />
+            ))}
+          </div>
+        </Tabs.Panel>
+
+        <Tabs.Panel value="saved">
+          <div className="mt-10 flex gap-5 flex-wrap px-20">
+            {jobs.map((job, index) => (
+              <Card key={index} {...job} saved />
+            ))}
+          </div>
+        </Tabs.Panel>
+        <Tabs.Panel value="offered">
+          <div className="mt-10 flex gap-5 flex-wrap px-20">
+            {jobs.map((job, index) => (
+              <Card key={index} {...job} offered />
+            ))}
+          </div>
+        </Tabs.Panel>
+        <Tabs.Panel value="interviewing">
+          <div className="mt-10 flex gap-5 flex-wrap px-20">
+            {jobs.map((job, index) => (
+              <Card key={index} {...job} interviewing />
+            ))}
+          </div>
+        </Tabs.Panel>
+      </Tabs>
+    </div>
+  );
+};
+
+export default JobHistory;
